55efc93b863f212b471db9c2673e12c1c82ed3e2
thelunacodes <delunacomunicacao@gmail.com>
2026-03-12 15:22:49 -0300

    Modal de Adicionar/Editar quase pronto

---
+++ games.js
@@ -0,0 +1,184 @@
+import { openCreateEditModal } from "./modal.js";
+
+class Game {
+    constructor(id,
+                image=undefined,
+                name,
+                developer=undefined,
+                publisher=undefined,
+                platform,
+                link=undefined,
+                startedAtDate=undefined,
+                startedAtTime,
+                finishedAtDate=undefined,
+                finishedAtTime,
+                score=undefined,) {
+        this.id = id;
+        this.image = image;
+        this.name = name;
+        this.developer = developer;
+        this.publisher = publisher;
+        this.platform = platform;
+        this.link = link;
+        this.startedAtDate = startedAtDate;
+        this.startedAtTime = startedAtTime;
+        this.finishedAtDate = finishedAtDate;
+        this.finishedAtTime = finishedAtTime;
+        this.score = score; 
+    }
+}
+
+function parseJsonToGameInstance(data) {
+    const gameInstance = new Game(data.id,
+                                data.img,
+                                data.name,
+                                data.developer,
+                                data.publisher,
+                                data.platform,
+                                data.link,
+                                data.started_at_date ? new Date(data.started_at_date) : null,
+                                data.started_at_time ? data.started_at_time : null,
+                                data.finished_at_date ? new Date(data.finished_at_date) : null,
+                                data.finished_at_time ? data.finished_at_time : null,
+                                data.score 
+    );
+
+    return gameInstance;
+}
+
+export async function loadGames() {
+    const response = await fetch("tempStorage.json");
+    const data = await response.json();
+
+    const gameList = data.games ?? [];
+
+    gameList.map(parseJsonToGameInstance)
+            .forEach(addGameToTable);
+}
+
+function addGameToTable(game) {
+    let newGameRow = document.createElement("tr")
+    newGameRow.className = "game-row";
+
+    const gameTable = document.getElementById("game-table");
+    gameTable.appendChild(newGameRow);
+
+    // Image
+    const image_src = `${!game.image ? 'assets/game_image_placeholder.png' : game.image}`
+    let image_cell = newGameRow.insertCell(0);
+    image_cell.innerHTML = `<img class='game-img' 
+                                src=${image_src}
+                                alt="${game.name}'s image">`
+                                        
+    //Name
+    let name_cell = newGameRow.insertCell(1)
+    name_cell.className = "game-name";
+    name_cell.innerHTML = `<p>${game.name}</p>`
+
+    //Developer
+    const dev_name = `${!game.developer ? '-' : `${game.developer}`}`
+
+    let dev_cell = newGameRow.insertCell(2)
+    dev_cell.className = "game-dev";
+    dev_cell.innerHTML = `<p>${dev_name}</p>`
+
+    //Publisher
+    const pub_name = `${!game.publisher ? '-' : `${game.publisher}`}`
+
+    let pub_cell = newGameRow.insertCell(3);
+    pub_cell.className = "game-publisher";
+    pub_cell.innerHTML = `<p>${pub_name}</p>`
+
+    //Platform
+    let plat_cell = newGameRow.insertCell(4);
+    plat_cell.className = "game-platform";
+    plat_cell.innerHTML = `<p>${game.platform}</p>`
+
+    //URL
+    const game_link = `${!game.link ? '-' : `<a target="_blank" href=${game.link}>${game.link}</a>`}`
+
+    let link_cell = newGameRow.insertCell(5);
+    link_cell.className = "game-link";
+    link_cell.innerHTML = `${game_link}`
+
+    //Started at (date/time)
+    let startedAt = "";
+
+    if (!game.startedAtDate && !game.startedAtTime) {
+        startedAt = "-"
+    } else {
+        let fullStartDate = "??/??/??";
+
+        if (game.startedAtDate) {
+            const startDay = String(game.startedAtDate.getDate()).padStart(2, '0');
+            const startMonth = String(game.startedAtDate.getMonth() + 1).padStart(2, '0');
+            fullStartDate = `${startDay}/${startMonth}/${game.startedAtDate.getFullYear()}`
+        } 
+
+        startedAt = `${fullStartDate} - ${!game.startedAtTime ? "??:??" : game.startedAtTime }`
+    }
+
+    let started_at_cell = newGameRow.insertCell(6)
+    started_at_cell.className = "game-started-at"
+    started_at_cell.innerHTML = `<p>${startedAt}</p>`
+
+    //Finished at (date/time)
+    let finishedAt = "" 
+
+    if (!game.finishedAtDate && !game.finishedAtTime) {
+        finishedAt = "-"
+    } else {
+        let fullFinishDate = "??/??/??";
+
+        if (game.finishedAtDate) {
+            const finishDay = String(game.finishedAtDate.getDate()).padStart(2, '0');
+            const finishMonth = String(game.finishedAtDate.getMonth() + 1).padStart(2, '0');
+            fullFinishDate = `${finishDay}/${finishMonth}/${game.finishedAtDate.getFullYear()}`
+        } 
+
+        const finishTime = !game.finishedAtTime ? "??:??" : game.finishedAtTime;
+
+        finishedAt = `${fullFinishDate} - ${finishTime}`
+    }
+
+    let finished_at_cell = newGameRow.insertCell(7)
+    finished_at_cell.className = "game-finished-at"
+    finished_at_cell.innerHTML = `<p>${finishedAt}</p>`
+
+    //Score
+    let classScore = ""
+
+    if (game.score <= 1) { // Ruim
+        classScore = "badScore";
+    } else if (game.score <= 2.5 ) { // Meh
+        classScore = "mehScore";
+    } else if (game.score <= 3.5) { // Ok
+        classScore = "okScore";
+    } else if (game.score <= 4.5) { // Bom
+        classScore = "goodScore";
+    } else if (game.score == 5.0) { // Muito Bom
+        classScore = "greatScore"
+    }
+
+    let class_cell = newGameRow.insertCell(8);
+    class_cell.className = "game-score"
+
+    const gameScore = game.score == null ? "-" : `${game.score.toFixed(1)}/5.0`;
+
+    class_cell.innerHTML = `<p class=${classScore}>${gameScore}</p>`
+
+    //Actions
+    let actions_cell = newGameRow.insertCell(9);
+    actions_cell.className = "game-actions"
+
+    let edit_btn = document.createElement("button")
+    edit_btn.className = "table-btn"
+    edit_btn.textContent = "Edit"
+    edit_btn.addEventListener("click", () => openCreateEditModal(game.id))
+
+    let remove_btn = document.createElement("button")
+    remove_btn.className = "table-btn"
+    remove_btn.textContent = "Remove"
+
+    actions_cell.append(edit_btn, remove_btn)
+}
--- modal.js
+++ modal.js
@@ -1,9 +1,111 @@
-function openModal(gameId=undefined) {
-    let modalContainer = document.getElementById("modal")
+// console.log("modal.js carregado")
 
+function openModal() {
+    let modal = document.getElementById("modal")
 
-    modalContainer.classList.remove("hidden");
-    modalContainer.classList.add("visible");
+    modal.classList.remove("hidden");
+    modal.classList.add("visible");
 
-    console.log("EBA")
+    // console.log("ABRIU")
+}
+
+export function closeModal() {
+    let modal = document.getElementById("modal")
+    
+    document.getElementById("game-form").reset();
+
+    toggleStartedFinishedAt("NOTPLAYED")
+
+    modal.classList.remove("visible");
+    modal.classList.add("hidden");
+
+    // console.log("FECHOU")
+}
+
+export function openCreateEditModal(gameId=undefined) {
+    // console.log(gameId);
+    openModal();
+
+    // Reset scroll
+    let modalContainer = document.getElementById("modal-container")
+    modalContainer.scrollTop = 0;
+
+    let modalHeader = document.getElementById("add-edit-header")
+
+    if (gameId) {
+        modalHeader.innerHTML = "Edit Game";
+    } else {
+        modalHeader.innerHTML = "Add Game";
+    }
+}
+
+//Add/Edit Game Form
+const gameForm = document.getElementById("game-form");
+
+gameForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let gameNameInput = document.getElementById("game-name");
+    let gameDevInput = document.getElementById("game-dev");
+    let gamePublisherInput = document.getElementById("game-publisher");
+    // let gamePlatformInput = document.getElementById("game-platform");
+    let gameLinkInput = document.getElementById("game-link");
+    // let gameStatusInput = document.getElementById("game-name");
+    let gameStartDateInput = document.getElementById("game-start-date");
+    let gameStartTimeHourInput = document.getElementById("game-start-hour");
+    let gameStartTimeMinuteInput = document.getElementById("game-start-minute");
+    let gameFinishDateInput = document.getElementById("game-finish-date");
+    let gameFinishTimeHourInput = document.getElementById("game-finish-hour");
+    let gameFinishTimeMinuteInput = document.getElementById("game-finish-minute");
+    // let gameScoreInput = document.getElementById("game-name");
+
+    console.log(`Name: ${gameNameInput.value}`)
+    console.log(`Developer: ${gameDevInput.value}`)
+    console.log(`Publisher: ${gamePublisherInput.value}`)
+    console.log(`Platform: `)
+    console.log(`Link: ${gameLinkInput.value}`)
+    console.log(`Start Date: ${gameStartDateInput.value}`)
+    console.log(`Start Hour: ${gameStartTimeHourInput.value}:${gameStartTimeMinuteInput.value}`)
+    console.log(`Finish Date: ${gameFinishDateInput.value}`)
+    console.log(`Finish Hour: ${gameFinishTimeHourInput.value}:${gameFinishTimeMinuteInput.value}`)
+    console.log(`Score: `)
+
+})
+
+gameForm.addEventListener("change", function(event) {
+    if (event.target.type === "radio") {
+        toggleStartedFinishedAt(event.target.value);
+    }
+})
+
+function toggleStartedFinishedAt(status) {
+    let startedAtInfo = document.getElementById("started-at-info")
+    let finishedAtInfo = document.getElementById("finished-at-info")
+
+    switch(status) {
+        case "NOTPLAYED":
+            startedAtInfo.classList.remove("visible");
+            startedAtInfo.classList.add("hidden");
+
+            finishedAtInfo.classList.remove("visible");
+            finishedAtInfo.classList.add("hidden");
+            break;
+        case "PLAYING":
+            startedAtInfo.classList.remove("hidden");
+            startedAtInfo.classList.add("visible");
+
+            finishedAtInfo.classList.remove("visible");
+            finishedAtInfo.classList.add("hidden");
+            break;
+        case "FINISHED":
+            startedAtInfo.classList.remove("hidden");
+            startedAtInfo.classList.add("visible");
+
+            finishedAtInfo.classList.remove("hidden");
+            finishedAtInfo.classList.add("visible");       
+    }
+}
+
+function onSubmitAddEditGame() {
+    
 }
