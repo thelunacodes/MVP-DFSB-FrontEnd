c7d0951b3c7cc653f40ff83ea25aba070d3cd7e8
thelunacodes <delunacomunicacao@gmail.com>
2026-03-12 00:49:10 -0300

    commit inicial

---
+++ modal.js
@@ -0,0 +1,9 @@
+function openModal(gameId=undefined) {
+    let modalContainer = document.getElementById("modal")
+
+
+    modalContainer.classList.remove("hidden");
+    modalContainer.classList.add("visible");
+
+    console.log("EBA")
+}
