0ed05ea14de3b91102e9dc602eb39cedb8c048de
gabriel.luna <gabriel.luna@fgv.br>
2026-03-27 15:18:52 -0300

    Terminar validação de data, fazer ajustes para obtenção de jogo por id e terminar de implementar edição

--- index.js
+++ index.js
@@ -104,6 +104,11 @@ function addGameToDatabase(game) {
         .catch((error) => console.error(error));
 }
 
+// Update game in database
+function updateGame(game) {
+
+}
+
 function removeRow(event, gameId, gameTitle) {
     if (confirm(`Are you sure you want to delete this game?\n\n'${gameTitle}' will be lost forever! (A long time!)`)) {
         const gameRow = event.target.closest("tr");
@@ -195,14 +200,34 @@ function createActions(row, game) {
 
     const edit = document.createElement("button");
     edit.textContent = "Edit";
-    edit.onclick = () => openCreateEditModal(game.id);
+    edit.onclick = () => openModal(game.id);
 
     const remove = document.createElement("button");
     remove.className = "gameRemoveBtn"
     remove.textContent = "Remove";
     remove.onclick = (event) => removeRow(event, game.id, game.gameTitle);
 
+    if (game.gameUrl) {
+        const goToPage = document.createElement("input");
+        goToPage.className = "goToPageBtn";
+        goToPage.type = "button";
+        goToPage.textContent = "Game Page"
+        
+    }
+
     cell.append(edit, remove);
+}
+
+function validateURL(str) {
+    let url;
+
+    try {
+        url = new URL(str);
+    } catch (_) {
+        return false;
+    }
+
+    return url.protocol === "http:" || url.protocol === "https:";
 }
 
 function addGameToTable(game) {
@@ -218,17 +243,6 @@ function addGameToTable(game) {
     createCell(newGameRow, "game-title", game.gameTitle);  // Name
     createCell(newGameRow, "game-dev", game.developer);  // Developer
     createCell(newGameRow, "game-platform", game.platform);  // Platform
-
-    //URL
-    if (!game.gameUrl) {
-        createCell(newGameRow, "game-url", "-");
-    } else {
-        const aGameUrl = document.createElement("a");
-        aGameUrl.target = "_blank";
-        aGameUrl.href = game.gameUrl;
-        aGameUrl.textContent = game.gameUrl;
-        createCell(newGameRow, "game-url", aGameUrl)
-    }
     
     createCell(newGameRow, "game-started-at", getDateTimeString(game.startDate, game.startTime))  //Started at (date/time)
     createCell(newGameRow, "game-finished-at", getDateTimeString(game.finishDate, game.finishTime))  //Finished at (date/time)
@@ -273,10 +287,11 @@ const modalHeader = $element("add-edit-header")
 const gamePlatformList = ["PC", "Playstation 1", "Playstation 2", "Playstation 3", "Playstation 4", "Playstation 5", "Playstation 6", "Xbox", "Xbox360", "Xbox One", "Xbox Series X/S", "NES", "SNES", "Nintendo 64", "GameCube", "Wii", "Wii U", "Game&Watch", "Gameboy", "Gameboy Color", "Gameboy Advance", "Nintendo DS", "Nintendo DSi", "Nintendo 3DS", "New Nintendo 3DS", "Virtual Boy", "Nintendo Switch", "Nintendo Switch 2", "Zeebo", "Atari 2600", "Atari 5200", "Atari 7800", "Atari XEGS", "Atari Lynx", "Atari Lynx II", "Atari Jaguar", "Atari VCS", "CD-i"]
 const gameScoreValues = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0];
 let hasScore = scoreToggleCheck.checked;
+let update = false;
 
 const formInputs = {
     gameImageUrl: $element("game-form-image_url"),
-    gametitle: $element("game-form-title"),
+    gameTitle: $element("game-form-title"),
     dev: $element("game-form-dev"),
     platform: $element("game-form-platform"),
     link: $element("game-form-link"),
@@ -291,26 +306,64 @@ function closeModal() {
     // Reset content
     gameForm.reset();
     imageExample.src = 'assets/game_image_placeholder.png';
+    formInputs.score.disabled = true;
     currentScore.textContent = `${gameScoreValues[0]}`; 
 
     modal.classList.remove("flex");
     modal.classList.add("hidden");
 }
 
-function openCreateEditModal(gameId=undefined) {
+function openModal(gameId=undefined) {
     modal.classList.remove("hidden");
     modal.classList.add("flex");
+    update = gameId != null;
+
+    console.log(update ? "UPDATE" : "CREATE")
 
     loadPlatforms();  
 
-    modalContainer.scrollTop = 0;
-
-
-    if (gameId) {
+    if (update) {
         modalHeader.innerHTML = "Edit Game";
-    } else {
+        // Get game from db with id ........
+        loadGameIntoForm(testGame);
+    } else {    
         modalHeader.innerHTML = "Add Game";
     }
+
+    modalContainer.scrollTop = 0;
+}
+
+function scoreCheck(game) {
+    if (game.score < 0 || game.score > 5) {
+        game.score = undefined;
+    }
+}
+
+function dateCheck(date) {
+    return date ? date.toISOString().split('T')[0] : undefined;
+}
+
+function timeCheck(time) {
+    const regex = /^([01]\d|2[0-3]):([0-5]\d)(:[0-5]\d)?$/;
+    return regex.test(time) ? time : undefined;
+}
+
+function loadGameIntoForm(game) {
+    formInputs.gameImageUrl.value = game.imageUrl || "";
+    toggleImageTest(); // Update example image (if needed)
+    formInputs.gameTitle.value = game.gameTitle || "";
+    formInputs.dev.value = game.developer || "";
+    formInputs.platform.value = game.platform || "";
+    formInputs.link.value = game.gameUrl || "";
+    formInputs.startDate.value = dateCheck(game.startDate);
+    formInputs.startTime.value = timeCheck(game.startTime);
+    formInputs.finishDate.value = dateCheck(game.finishDate);
+    formInputs.finishTime.value = timeCheck(game.finishTime);
+    scoreCheck(game); // Score value validation
+    formInputs.score.value = gameScoreValues.indexOf(game.score) || 0;
+    currentScore.textContent = game.score || 0;
+    formInputs.score.disabled = game.score == null;
+    scoreToggleCheck.checked = game.score != null;
 }
 
 function loadPlatforms() {
@@ -332,16 +385,42 @@ function loadPlatforms() {
 }
 
 function toggleImageTest() {
-    imageExample.src = formInputs.gameImageUrl.value || 'assets/game_image_placeholder.png';
+    imageExample.src = validateURL(formInputs.gameImageUrl.value) ? formInputs.gameImageUrl.value : 'assets/game_image_placeholder.png';
 } 
 
 gameForm.addEventListener("submit", function(event) {
     event.preventDefault();
+    const invalidURLMsg = (fieldName) => `"Oops! The URL you've entered at '${fieldName}' is invalid! Please enter a valid URL or leave it empty."`
+
+    // URL Validation
+    if (formInputs.gameImageUrl.value.trim() != "" && !validateURL(formInputs.gameImageUrl.value)) {
+        alert(invalidURLMsg("Game Image (URL)"))
+        return;
+    }
+
+    if (formInputs.link.value.trim() != "" && !validateURL(formInputs.link.value)) {
+        alert(invalidURLMsg("Link (URL)"))
+        return;
+    }
+
+    // Date/Time Validation
+    const startDateValue = new Date(formInputs.startDate.value);
+    const startTimeValue = new Date(formInputs.startTime.value);
+    const finishDateValue = new Date(formInputs.finishDate.value);
+    const finishTimeValue = new Date(formInputs.finishTime.value);
+
+    if (finishDateValue < startDateValue) {
+        alert("The 'Finished At' date must be after the 'Started At' date.")
+    } else if (startDateValue.getTime() == finishDateValue.getTime()) {
+        console.log("equal")
+        console.log(startTimeValue)
+        console.log(finishTimeValue)
+    }
 
     let newGame = new Game(
         undefined,
         formInputs.gameImageUrl.value,
-        formInputs.gametitle.value,
+        formInputs.gameTitle.value,
         formInputs.dev.value,
         formInputs.platform.value,
         formInputs.link.value,
@@ -352,7 +431,8 @@ gameForm.addEventListener("submit", function(event) {
         hasScore ? gameScoreValues[formInputs.score.value] : null
     );
 
-    addGameToDatabase(newGame);
+    console.log("GAME ADDED YAY!")
+    // addGameToDatabase(newGame);
 })
 
 formInputs.score.addEventListener("input", () => {
@@ -365,4 +445,20 @@ scoreToggleCheck.addEventListener("change", (event) => {
 
 });
 
-getGameList();  // Retrieve games 
+// getGameList();  // Retrieve games 
+
+const testGame = new Game(
+    1, 
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZbB_doR9LVg_xVbDXOOZc3TNbgNCEIzLLKw&s",
+    "Orange: The game",
+    "Adam Sandler",
+    "Zeebo",
+    "https://www.google.com",
+    undefined,
+    "22:30",
+    undefined,
+    undefined,
+    2.0
+)
+
+addGameToTable(testGame);
